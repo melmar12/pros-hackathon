3396f13683a202ad85f01f2a597d43ed3b64dc7b
Donovan Preston <dpreston@pros.com>
2020-06-30 16:42:13 -0500

    style updates to flight container

--- client/src/components/flight-container.js
+++ client/src/components/flight-container.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './flight-container.css'
 
 class FlightContainer extends React.Component{
     constructor(props){
@@ -14,9 +15,9 @@ class FlightContainer extends React.Component{
 
     render(){
         return(
-        <Card>
-        <Card.Header>{this.airline}</Card.Header>
-        <Card.Body>
+        <Card className="FlightContainer">
+        <Card.Body className="Header">{this.airline}</Card.Body>
+        <Card.Body className="Body">
         <Card.Title>{this.currentLocation} - {this.destination}</Card.Title>
         <Card.Text>{this.type}</Card.Text>
         </Card.Body>
